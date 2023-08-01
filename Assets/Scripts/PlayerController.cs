c55c42ade7c6b78d6e437ac52e3b0a209b279944
Dawid Bieńkowski <dbienkowski@op.pl>
2023-08-01 23:57:14 +0200

    Creating and moving the player
    Created the player character and implemented its moving system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+
+        rb.velocity = moveInput * moveSpeed;
+    }
+}
