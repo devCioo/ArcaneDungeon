d9f06d6766490d082b881317d938953515ac4cc0
Dawid Bieńkowski <dbienkowski@op.pl>
2023-08-02 22:49:33 +0200

    Adding spikes and fixing sorting layers
    Added floor spikes along with their retractable version and assigned existing objects to their respective layers

---
+++ Assets/Scripts/FloorSpikes.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSpikes : MonoBehaviour
+{
+    public bool areRetractable;
+    private bool canDamage;
+    private float hiddenTime = 1.5f, shownTime = 1.5f;
+    private float hiddenShownDeltaTime = 0.15f;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canDamage = true;
+
+        if (areRetractable)
+        {
+            StartCoroutine(HideAndShow());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (canDamage)
+            {
+                PlayerHealthController.instance.TakeDamage(1);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (canDamage)
+            {
+                PlayerHealthController.instance.TakeDamage(1);
+            }
+        }
+    }
+
+    public IEnumerator HideAndShow()
+    {
+        anim.Play("Retractable_Spikes_Hide");
+        canDamage = false;
+
+        for (float i = 0f; i < hiddenTime; i += hiddenShownDeltaTime)
+        {
+            yield return new WaitForSeconds(hiddenShownDeltaTime);
+        }
+
+        canDamage = true;
+        anim.Play("Retractable_Spikes_Show");
+
+        for (float i = 0f; i < shownTime; i += hiddenShownDeltaTime)
+        {
+            yield return new WaitForSeconds(hiddenShownDeltaTime);
+        }
+
+        StartCoroutine(HideAndShow());
+    }
+}
