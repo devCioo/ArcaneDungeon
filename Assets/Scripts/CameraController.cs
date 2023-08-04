0c240402398ecd916442eee2547f2344f62e1abc
Dawid Bieńkowski <dbienkowski@op.pl>
2023-08-04 11:14:27 +0200

    Adding tileset and creating basic rooms
    Added tileset to create rooms, two basic rooms and a camera's transition on changing rooms

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public float transitionSpeed;
+
+    public Transform target;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), transitionSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ChangeTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
